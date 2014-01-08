f7852b2b8b7f3db7ec08b43e081bfa80bba577b9
kc123kc <klc1630@126.com>
2014-01-08 23:27:29 +0800

    Add test

---
+++ server/tests/connectionTest.js
@@ -0,0 +1,24 @@
+var should = require('should');
+var User = require('../models/User');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/local');
+
+describe('User', function(){
+
+    beforeEach(function(done){
+        //clean the database:
+        User.remove(done);
+    });
+
+    describe('#save()', function() {
+        it('should save', function(done) {
+            var user = new User({userName: 'gmart' })
+            user.save(function(err) {
+                if (err) return done(err);
+                user.should.have.property('userName','gmart');
+                done();
+            });
+        });
+    });
+});
