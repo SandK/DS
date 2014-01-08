8c0e26eab6db42b8b84c80e39613bd20642995db
kc123kc <klc1630@126.com>
2014-01-08 22:40:16 +0800

    Del All.Start from zero

--- server/demos/kctest/hello.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-   
-http.createServer( function (req, res) {
-  res.writeHead( 200 , {"Content-Type": "text/html"});
-  res.write("<h1>Node.js</h1>");
-  res.write("<p>Hello World</p>");
-  res.end("<p>kc</p>");
-}).listen(3000);
-console.log("HTTP server is listening at port 3000.");
