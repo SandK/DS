c9c002e579754f216d6ad58091bf74ff8f4b23e5
root <root@ip-172-31-3-112.us-west-2.compute.internal>
2014-03-04 14:21:02 +0000

    modify user info update

--- server/routes/user.js
+++ server/routes/user.js
@@ -59,8 +59,10 @@ exports.updateUserInfo = function(req, res) {
 	}
 	var id = req.params.id;
 	delete req.user._id;
-	var user = req.user;
-	logger.info("updateUser id: %s", id);
+	var user = {};
+	user.nickname = req.user.nickname;
+	user.age = req.user.age;
+	logger.info("updateUser id: %s, nickname: %s, age: %s", id, req.user.nickname, req.user.age);
 
 	// 更新用户数据库信息
 	var updateUser = function() {
