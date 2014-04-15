7856e820c784e98116a3f0184134aafaa8214f8c
sam <289623783@qq.com>
2014-04-15 15:50:49 +0800

    增加：瀑布流排版初体验

--- web/js/modules/task/controllers/taskC.js
+++ web/js/modules/task/controllers/taskC.js
@@ -1,10 +1,65 @@
 define(['modules/ds'], function(ds) {
 	ds.controller('TaskController', function ($scope, $http) {
-		$scope.tasklist = [{
-			title: "title",
-			tip: "tip",
-			reward: "reward"	
-		}]
+		$scope.tasklist = (function() {
+			var arr = [];
+			var tip = [
+				" onec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ",
+				"onec id Donec id Donec id Donec id Don",
+				"onec id Donec id Donec id Donec id Dononec id Donec id Donec id Donec id Dononec id Donec id Donec id Donec id Dononec id Donec id Donec id Donec id Dononec id Donec id Donec id Donec id Don"
+			]
+			for (var i = 0; i < 3; ++i) {
+				arr.push([{
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}, {
+					title: "title",
+					tip: tip[Math.floor(Math.random() * 3)],
+					reward: "dasdasdasd"
+				}]);
+			}
+			return arr;
+		})();
 
 	  	var reset = function() {
 			
