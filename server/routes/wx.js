90b50bfb91d208cff357dd8f23f57071f078e899
kc123kc <klc1630@126.com>
2014-06-04 07:50:38 +0800

    公众帐号测试

--- server/routes/wx.js
+++ server/routes/wx.js
@@ -19,24 +19,75 @@ module.exports.handleMsg = wechat(config.token,
 
 wechat.text(function(message, req, res, next) {
 	console.log(message);
+	var input = (message.Content || '').trim();
+	var content = '';
+	// Logic Handle
+	if (input == '图文')
+	{
+		res.reply([
+			{
+				title: '测试图文消息',
+				description: '图文',
+				picurl: 'http://su.bdimg.com/static/superplus/img/logo_white_2a2fcb5a.png',
+				url: 'http://www.baidu.com/'
+			}
+		]);
+	}
+	else
+	{
+		res.reply("我收到了|" + input);
+	}
 })
 .image(function (message, req, res, next) {
 	console.log(message);
+	res.reply({
+		type: "image",
+		content: {
+			mediaId: message.MediaId
+		}
+	});
 })
 .voice(function (message, req, res, next) {
 	console.log(message);
+	res.reply({
+		type: "voice",
+		content: {
+			mediaId: message.MediaId
+		}
+	});
 })
 .video(function (message, req, res, next) {
 	console.log(message);
+	res.reply({
+		type: "video",
+		content: {
+			mediaId: message.MediaId,
+    		thumbMediaId: message.ThumbMediaId
+		}
+	});
 })
 .location(function (message, req, res, next) {
 	console.log(message);
+	res.reply("我收到了位置|" + message.Location_X + "|" + message.Location_Y 
+		 "|" + message.Scale);
 })
 .link(function (message, req, res, next) {
 	console.log(message);
 })
 .event(function (message, req, res, next) {
 	console.log(message);
+	if (message.Event === 'subscribe')
+	{
+		res.reply("欢迎添加许愿墙");
+	}
+	else if (message.Event === 'unsubscribe')
+	{
+		res.reply("bye");
+	}
+	else
+	{
+		res.reply("暂未支持");
+	}
 })
 
 );
